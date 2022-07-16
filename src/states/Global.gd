44fad8b1947bef5869293e51fc619ae2e03e81b0
jokertyf <admoore@wpi.edu>
2022-07-16 16:28:23 -0400

    Added music and options.

--- src/states/Global.gd
+++ src/states/Global.gd
@@ -1,4 +1,29 @@
 extends Node
 
 
+const MAX_VOLUME_DB = -6.0
+
 var level_path: String
+
+var bg_music = preload("res://assets/sound/Xetator.wav")
+
+# Options
+var music_slider_value = 1.0
+var music_volume = 1.0
+var animation_speed = 2.0
+
+var audio_stream: AudioStreamPlayer
+
+func _ready() -> void:
+	audio_stream = AudioStreamPlayer.new()
+	audio_stream.stream = bg_music
+	audio_stream.pause_mode = Node.PAUSE_MODE_PROCESS
+	audio_stream.volume_db = MAX_VOLUME_DB
+	audio_stream.play()
+	add_child(audio_stream)
+
+
+func set_music_volume(volume: float) -> void:
+	# Volume is given as a percent, so change that to dB.
+	music_slider_value = volume
+	audio_stream.volume_db = MAX_VOLUME_DB + (log(volume) / log(1.1))
--- src/states/Menu.gd
+++ src/states/Menu.gd
@@ -6,7 +6,6 @@ func _ready() -> void:
 		$V/Buttons/QuitButton.hide()
 	$V/Buttons/PlayButton.grab_focus()
 
-
 func _on_PlayButton_pressed() -> void:
 	Global.level_path = "res://src/levels/IntroLevel.tscn"
 	var error := get_tree().change_scene("res://src/states/Game.tscn")
@@ -19,3 +18,12 @@ func _on_CreditsButton_pressed() -> void:
 
 func _on_QuitButton_pressed() -> void:
 	get_tree().notification(MainLoop.NOTIFICATION_WM_QUIT_REQUEST)
+
+
+func _on_OptionsButton_pressed() -> void:
+	$V.hide()
+	$OptionsMenu/ColorRect.show()
+
+
+func _on_OptionsMenu_options_exited() -> void:
+	$V.show()
