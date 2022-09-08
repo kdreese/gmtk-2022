34df7b979d3b04da9617044c2ed91d5544c67d16
Terry Hearst <thearst3rd@gmail.com>
2022-09-08 19:48:58 -0400

    Toggle fullscreen with Alt+Enter (like every other game)

--- src/scripts/Global.gd
+++ src/scripts/Global.gd
@@ -140,7 +140,7 @@ func _notification(what: int) -> void:
 		get_tree().quit()
 
 
-func _unhandled_input(event: InputEvent) -> void:
+func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = not OS.window_fullscreen
 		save_config()
